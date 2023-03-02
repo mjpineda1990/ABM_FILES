8856568318fa2c7bf01a02a944e50f0f9299895a
pherzxc@gmail.com <pherzxc@gmail.com>
2023-03-01 21:21:25 -0700

    adding john code here

--- Csharp Practice/string ReverseMJ.cs
+++ Csharp Practice/string ReverseMJ.cs
@@ -6,6 +6,10 @@ public class HelloWorld
     {
        string greetings = "Hello my name is MJ";
        reverseString(greetings);
+
+        string john = "Hello my name is John";
+        Reverse(john);
+
     }
     public static void reverseString(string name)
     {
@@ -16,5 +20,21 @@ public class HelloWorld
          Console.WriteLine("This is Original:"+"\n"+name);
          Console.WriteLine(" ");
         Console.WriteLine("This is Reverse:"+"\n"+reverseS);
+    }
+
+    public static void Reverse(string name)
+    {
+        string[] nameArr = name.Split(" ");
+        string reverseString = string.Empty;
+
+        for (int x = 0; x <= nameArr.Length - 1; x++)
+        {
+            for (int y = nameArr[x].Length - 1; y >= 0; y--)
+            {
+                reverseString += nameArr[x][y];
+            }
+            reverseString += " ";
+        }
+        Console.Write($"Reverse String is : {reverseString} ");
     }
 }
